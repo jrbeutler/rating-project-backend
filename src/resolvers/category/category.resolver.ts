79a7c4313399a8f4a1dbc3ee62d657e31a189146
Eli Sokeland <etsokeland@bsu.edu>
2020-12-09 20:57:16 -0600

    Able to Get All Categories

--- src/resolvers/category/category.resolver.ts
+++ src/resolvers/category/category.resolver.ts
@@ -1,5 +1,5 @@
 import { Category } from '../../models/category.model';
-import { Mutation, Resolver, Args } from '@nestjs/graphql';
+import { Mutation, Resolver, Args, Query } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from '../../guards/gql-auth.guard';
 import { CategoryService } from '../../services/category.service';
@@ -15,5 +15,10 @@ export class CategoryResolver {
     @CategoryEntity() category: Category,
     @Args('name') categoryName: string) {
     return this.categoryService.createCategory(categoryName);
+  }
+
+  @Query(() => [Category])
+  async getAllCategories() {
+    return this.categoryService.getAllCategories();
   }
 }
--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { Category } from '../models/category.model';
 
 @Injectable()
 export class CategoryService {
   constructor(
     private prisma: PrismaService,
   ) {}
+
+  getAllCategories(): Promise<Category[]> {
+    return this.prisma.category.findMany();
+  }
 
   createCategory(categoryName: string) {
     return this.prisma.category.create({
