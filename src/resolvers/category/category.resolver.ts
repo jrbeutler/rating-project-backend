fadda0ae2fef7af5bdaa7fa0e31b97925e03e5c3
Eli Sokeland <etsokeland@bsu.edu>
2021-04-06 23:33:30 -0400

    Can Now Archive Categories

--- src/resolvers/category/category.resolver.ts
+++ src/resolvers/category/category.resolver.ts
@@ -4,6 +4,8 @@ import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from '../../guards/gql-auth.guard';
 import { CategoryService } from '../../services/category.service';
 import { CategoryEntity } from '../../decorators/category.decorator';
+import {User} from "../../models/user.model";
+import {UserEntity} from "../../decorators/user.decorator";
 
 @Resolver(() => Category)
 export class CategoryResolver {
@@ -27,5 +29,21 @@ export class CategoryResolver {
   @CategoryEntity() category: Category,
   @Args('categoryID') categoryID: string) {
     return this.categoryService.getByID(categoryID);
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Mutation(() => Category)
+  async archiveCategory(
+      @UserEntity() category: Category,
+      @Args('categoryID') categoryID: string): Promise<Category> {
+    return this.categoryService.archiveCategory(categoryID);
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Mutation(() => Category)
+  async activateCategory(
+      @UserEntity() category: Category,
+      @Args('categoryID') categoryID: string): Promise<Category> {
+    return this.categoryService.activateCategory(categoryID);
   }
 }
